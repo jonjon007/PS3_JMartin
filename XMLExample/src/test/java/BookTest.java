ba2c29f865a47aa199f51171aa70cea1cdecbf4b
Jon-Jon <Jon-Jon@128.4.107.216>
2017-03-15 12:44:06 -0400

    Further revisions. Improved test cases.

--- XMLExample/src/test/java/BookTest.java
+++ XMLExample/src/test/java/BookTest.java
@@ -11,14 +11,14 @@ import pkgLibrary.BookException;
 public class BookTest {
 	@Test
 	public void Equals1(){
-		Book bk = new Book("bk099", "Me", "Thish", "Goodness", 100.00, new Date(1, 2, 3, 4, 5), "Pure Goodness");
-		Book mcDonalds = new Book("bk099", "Me", "Thish", "Goodness", 100.00, new Date(1, 2, 3, 4, 5), "Pure Goodness");
+		Book bk = new Book("bk097", "Me", "Thish", "Goodness", 100.00, new Date(1, 2, 3, 4, 5), "Pure Goodness");
+		Book mcDonalds = new Book("bk097", "Me", "Thish", "Goodness", 100.00, new Date(1, 2, 3, 4, 5), "Pure Goodness");
 		assertTrue(bk.equals(mcDonalds));
 	}
 	
 	@Test
 	public void Equals2(){
-		Book bk = new Book("bk099", "Me", "Thish", "Goodness", 100.00, new Date(1, 2, 3, 4, 5), "Pure Goodness");
+		Book bk = new Book("bk097", "Me", "Thish", "Goodness", 100.00, new Date(1, 2, 3, 4, 5), "Pure Goodness");
 		Book wendys = new Book("bk098", "Me", "Thish", "Goodness", 100.00, new Date(1, 2, 3, 4, 5), "Pure Goodness");
 		assertFalse(bk.equals(wendys));
 	}
@@ -31,8 +31,8 @@ public class BookTest {
 	
 	@Test
 	public void GetBook2() {
-		Book bk = new Book("bk105");
-		assertNotEquals(bk.GetBook("id099"), bk);
+		Book bk = new Book("bk110");
+		assertNotEquals(bk.GetBook("id110"), bk);
 	}
 	
 	@Test
